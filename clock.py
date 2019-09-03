431269eba8c9cab788088378a7fb3d849a8a0db5
karekear <karekear@gmail.com>
2019-09-03 18:13:11 +0900

    edited clock.py

--- clock.py
+++ clock.py
@@ -2,15 +2,13 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import datetime
 import deathcount
 
-"""
 twische = BlockingScheduler()
 
-@twische.scheduled_job('interval',minutes=5)
+@twische.scheduled_job('interval',minutes=1)
 def timed_job():
     deathcount.job()
 
 if __name__ == "__main__":
     twische.start()
-"""
 
-deathcount.job()
+#deathcount.job()
