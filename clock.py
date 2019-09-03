1bb3afb5c6e0b7b255781b16020e4707ce5e4c43
karekear <karekear@gmail.com>
2019-09-03 22:51:09 +0900

    cron: minutes -> minute

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ import deathcount
 twische = BlockingScheduler()
 
 #@twische.scheduled_job('interval',minutes=1)
-@twische.scheduled_job('cron',minutes=0) #毎時00分に実行
+@twische.scheduled_job('cron',minute=0) #毎時00分に実行
 def timed_job():
     deathcount.job()
 
